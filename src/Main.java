e77d5bb4b4fc0ac274f4caa1e39dc92045c2ddc6
kuba <Kudaiberdigapyrov@gmail.com>
2023-11-06 21:11:17 +0600

    firstkuba43

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Posword posword = new Posword(634354, "Kudayberdi", "Gapurov",
+                "Kyrgyzstan", "Myjskoy", "29.08.2006");
+
+        System.out.println("фамилия : " + posword.lastNAme +
+                "\n" + "имя : " + posword.firstName + "\n" + "айди : " + posword.id +
+                "\n" + "дата : " + posword.data + "\n" +
+                "страна :" + posword.country + "\n" + "пол : " + posword.gender);
+
     }
 }
